802d7a6aca4d80395dd75f720a5d79659fea813a
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-11-09 09:25:34 -0300

    projeto

---
+++ pages/about.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="navegacao">
+      <p><NuxtLink class="navegacao" to="/">Início</NuxtLink> > Sobre</p>
+    </div>
+    <img
+      class="banner-sobre"
+      src="~/assets/img/cine.jpg"
+      alt="Capa do Filme Sangue de Pelicano, Uma mulher que treina cavalos policiais quer mais um filho e então adota uma criança, uma menina de 5 anos gravemente traumatizada. Quando a menina mostra comportamento violento e antissocial, sua nova mãe fica determinada a ajudá-la."
+    />
+    <p>
+      Com Capacidade de 160 Lugares o Cinemec é um ótimo lugar para passar um
+      tempo com a familia ou com os amigos. <br />
+      Meia entrada: crianças de 03 à 12 anos. Acima de 12, somente com a
+      carteirinha escolar na validade.
+    </p>
+    <p>Como chegar ate o Cinema.</p>
+    <div class="map">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14858.39169020325!2d-48.5052161!3d-21.4057308!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcc662afc91651067!2sCinemec%20Taquaritinga!5e0!3m2!1spt-BR!2sbr!4v1602610559177!5m2!1spt-BR!2sbr"
+        width="400"
+        height="300"
+        frameborder="0"
+        style="border: 0"
+        allowfullscreen=""
+        aria-hidden="false"
+        tabindex="0"
+      ></iframe>
+    </div>
+  </div>
+</template>
---
+++ pages/tickets.vue
@@ -0,0 +1,87 @@
+<template>
+<form id="formExemplo" data-toggle="validator" role="form">	
+  <div class="form">
+    <div class="navegacao">
+      <p>
+        <NuxtLink class="navegacao" to="/">Início</NuxtLink> > Reserve seu lugar
+      </p>
+    </div>
+  <div class="form">
+      <label for="validationCustom01">Nome:</label>
+      <input type="text" class="form-control" id="validationCustom01" required>
+        </div>
+    <div class="form">
+      <label for="validationCustom02">Quantidade de Pessoas:</label>
+      <input type="text" class="form-control" id="validationCustom02" required>
+       </div>
+    <div class="form">
+   <select class="custom-select">
+  <option selected>Selecione um Filme:</option>
+  <option value="1">Escolhida</option>
+  <option value="2">Morte Súbita 2</option>
+  <option value="3">A Gangue: Uma noite de sangue</option>
+  <option value="4">Sangue Pelicano</option>
+</select>
+      </div>
+  <div class="form">
+    <b-form-group id="input-group-4" label="Deseja assitir em 3D:" label-for="input-4">
+      <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
+        
+      </b-form-checkbox-group>
+      <label>	        
+        <input type="checkbox" data-error="Você deve marcar este campo!" required> Sim</label>
+        <input type="checkbox" data-error="Você deve marcar este campo!" required><label> Não </label>
+
+    </b-form-group>
+    </div>
+    <b-button type="submit" variant="primary">Enviar</b-button>
+  </div>
+</form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        name: "",
+        food: null,
+        checked: [],
+      },
+      foods: [
+        { text: "Selecione", value: null },
+        "Escolhida",
+        "Morte Súbita 2",
+        "A Gangue: Uma noite de sangue",
+        "Sangue de Pelicano",
+      ],
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
+</script>
+<<<<<<< HEAD
+
+
+=======
+>>>>>>> parent of 2c0040c4... tickets.vue
