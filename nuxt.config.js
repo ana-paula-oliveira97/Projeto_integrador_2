f84659fe8a958746e7956d2e01f8bf4af7462f1b
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-11-17 09:03:16 -0300

    estilo e configuraçãoes

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Confira os Nossos Filmes em Exibição e Venha Assistir com os Amigos Ainda Hoje. o cinema com os melhores filmes para você e a familia.' },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: 'cinema,filme',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
