224710e92fc95b1d3d675d5d5d77e84f1ca229e5
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-11-26 18:03:39 -0300

    ajustes do sobre

--- pages/about.vue
+++ pages/about.vue
@@ -13,27 +13,29 @@
     <p class="txt-sobre">
       Confira o video com os Filmes em cartaz e como chegar ao Cinema.
     </p>
-    <div class="video-sobre">
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/PV_z1x5DckE"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-
-      <iframe
-        class="map"
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14858.39169020325!2d-48.5052161!3d-21.4057308!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcc662afc91651067!2sCinemec%20Taquaritinga!5e0!3m2!1spt-BR!2sbr!4v1602610559177!5m2!1spt-BR!2sbr"
-        width="400"
-        height="300"
-        frameborder="0"
-        style="border: 0"
-        allowfullscreen=""
-        aria-hidden="false"
-        tabindex="0"
-      ></iframe>
+    <div class="sobre">
+      <div class="trailer">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/B0VL51LFe3k?start=13"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div class="map">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14858.39169020325!2d-48.5052161!3d-21.4057308!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcc662afc91651067!2sCinemec%20Taquaritinga!5e0!3m2!1spt-BR!2sbr!4v1602610559177!5m2!1spt-BR!2sbr"
+          width="400"
+          height="300"
+          frameborder="0"
+          style="border: 0"
+          allowfullscreen=""
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
+      </div>
     </div>
   </div>
 </template>
--- pages/poster.vue
+++ pages/poster.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="conterner-p">
+  <div class="conterner">
     <div class="card">
       <div class="navegacao">
         <p>
@@ -7,98 +7,95 @@
           cartaz
         </p>
       </div>
-      <div class="card">
-        <b-card-group deck
-          ><div class="filme1-p">
-            <b-card
-              class="1"
-              title="Convenção das Bruxas"
-              img-src="~/assets/img/bruxas.jpg"
-              img-alt="Capa do Filme Convenção das Bruxas ,Um menino acaba descobrindo uma conferência de bruxas enquanto fica com a avó em um hotel e é transformado em rato pela Grande Bruxa."
-              img-top
-            >
-              <b-card-text>
-                Um menino acaba descobrindo uma conferência de bruxas enquanto
-                fica com a avó em um hotel e é transformado em rato pela Grande
-                Bruxa.<br />
-                remaster do classico filme de 1990.
-                <p>&#32;</p>
-              </b-card-text>
-              <template v-slot:footer>
-                <b-button class="botao" variant="Primary"
-                  ><NuxtLink to="/the_witches">Ver mais </NuxtLink></b-button
-                >
-              </template>
-            </b-card>
-          </div>
-          <div class="filme2-p">
-            <b-card
-              class="2"
-              title="Freaky"
-              img-src="~/assets/img/Freaky.jpg"
-              alt="Capa do Filme Freaky: Depois de trocar de corpo com um assassino em série, a adolescente de colegial Millie descobre que tem menos de um dia para reverter a situação antes que seja tarde demais."
-              img-top
-            >
-              <b-card-text>
-                Depois de trocar de corpo com um assassino em série, a
-                adolescente de colegial Millie descobre que tem menos de um dia
-                para reverter a situação antes que seja tarde demais.
-                <p>&#32;</p>
-              </b-card-text>
-              <template v-slot:footer>
-                <b-button class="botao" variant="Primary"
-                  ><NuxtLink to="/freaky">Ver mais </NuxtLink></b-button
-                >
-              </template>
-            </b-card>
-          </div>
 
-          <div class="filme3-p">
-            <b-card
-              class="3"
-              title="Tenet"
-              img-src="~/assets/img/tenet.jpg"
-              img-alt="Capa do Filme Tenet: Em um mundo crepuscular da espionagem internacional, um agente da CIA sem nome, conhecido como o protagonista, é recrutado por uma organização misteriosa chamada Tenet para participar de uma missão global que se desdobra além do tempo real. "
-              img-top
-            >
-              <b-card-text>
-                Armado com apenas uma palavra 'Tenet' e lutando pela
-                sobrevivência do mundo inteiro, o Protagonista viaja através de
-                um mundo crepuscular de espionagem internacional em uma missão
-                que irá desenrolar em algo para além do tempo real.
-              </b-card-text>
-              <template v-slot:footer>
-                <b-button class="botao" variant="Primary"
-                  ><NuxtLink to="/tenet">Ver mais </NuxtLink>
-                </b-button>
-              </template>
-            </b-card>
-          </div>
-          <div class="filme4-p">
-            <b-card
-              class="4"
-              title="Os novos mutantes"
-              img-src="~/assets/img/os novos mutantes.jpg"
-              img-alt="Capa do Filme Os novos mutantes,Cinco jovens mutantes descobrem o alcance de seus poderes e lidam com traumas do passado enquanto são mantidos presos contra a vontade num sinistro hospital."
-              img-top
-            >
-              <b-card-text>
-                Cinco jovens mutantes descobrem o alcance de seus poderes e
-                lidam com traumas do passado enquanto são mantidos presos contra
-                a vontade num sinistro hospital.
-                <p>&#32;</p>
-              </b-card-text>
-              <template v-slot:footer>
-                <b-button class="botao" variant="Primary"
-                  ><NuxtLink to="/the_new_mutants"
-                    >Ver mais
-                  </NuxtLink></b-button
-                >
-              </template>
-            </b-card>
-          </div>
-        </b-card-group>
-      </div>
+      <b-card-group deck
+        ><div class="filme1-p">
+          <b-card
+            class="1"
+            title="Convenção das Bruxas"
+            img-src="~/assets/img/bruxas.jpg"
+            img-alt="Capa do Filme Convenção das Bruxas ,Um menino acaba descobrindo uma conferência de bruxas enquanto fica com a avó em um hotel e é transformado em rato pela Grande Bruxa."
+            img-top
+          >
+            <b-card-text>
+              Um menino acaba descobrindo uma conferência de bruxas enquanto
+              fica com a avó em um hotel e é transformado em rato pela Grande
+              Bruxa.<br />
+              remaster do classico filme de 1990.
+              <p>&#32;</p>
+            </b-card-text>
+            <template v-slot:footer>
+              <b-button class="botao" variant="Primary"
+                ><NuxtLink to="/the_witches">Ver mais </NuxtLink></b-button
+              >
+            </template>
+          </b-card>
+        </div>
+        <div class="filme2-p">
+          <b-card
+            class="2"
+            title="Freaky"
+            img-src="~/assets/img/Freaky.jpg"
+            alt="Capa do Filme Freaky: Depois de trocar de corpo com um assassino em série, a adolescente de colegial Millie descobre que tem menos de um dia para reverter a situação antes que seja tarde demais."
+            img-top
+          >
+            <b-card-text>
+              Depois de trocar de corpo com um assassino em série, a adolescente
+              de colegial Millie descobre que tem menos de um dia para reverter
+              a situação antes que seja tarde demais.
+              <p>&#32;</p>
+            </b-card-text>
+            <template v-slot:footer>
+              <b-button class="botao" variant="Primary"
+                ><NuxtLink to="/freaky">Ver mais </NuxtLink></b-button
+              >
+            </template>
+          </b-card>
+        </div>
+
+        <div class="filme3-p">
+          <b-card
+            class="3"
+            title="Tenet"
+            img-src="~/assets/img/tenet.jpg"
+            img-alt="Capa do Filme Tenet: Em um mundo crepuscular da espionagem internacional, um agente da CIA sem nome, conhecido como o protagonista, é recrutado por uma organização misteriosa chamada Tenet para participar de uma missão global que se desdobra além do tempo real. "
+            img-top
+          >
+            <b-card-text>
+              Armado com apenas uma palavra 'Tenet' e lutando pela sobrevivência
+              do mundo inteiro, o Protagonista viaja através de um mundo
+              crepuscular de espionagem internacional em uma missão que irá
+              desenrolar em algo para além do tempo real.
+            </b-card-text>
+            <template v-slot:footer>
+              <b-button class="botao" variant="Primary"
+                ><NuxtLink to="/tenet">Ver mais </NuxtLink>
+              </b-button>
+            </template>
+          </b-card>
+        </div>
+        <div class="filme4-p">
+          <b-card
+            class="4"
+            title="Os novos mutantes"
+            img-src="~/assets/img/os novos mutantes.jpg"
+            img-alt="Capa do Filme Os novos mutantes,Cinco jovens mutantes descobrem o alcance de seus poderes e lidam com traumas do passado enquanto são mantidos presos contra a vontade num sinistro hospital."
+            img-top
+          >
+            <b-card-text>
+              Cinco jovens mutantes descobrem o alcance de seus poderes e lidam
+              com traumas do passado enquanto são mantidos presos contra a
+              vontade num sinistro hospital.
+              <p>&#32;</p>
+            </b-card-text>
+            <template v-slot:footer>
+              <b-button class="botao" variant="Primary"
+                ><NuxtLink to="/the_new_mutants">Ver mais </NuxtLink></b-button
+              >
+            </template>
+          </b-card>
+        </div>
+      </b-card-group>
     </div>
   </div>
 </template>
